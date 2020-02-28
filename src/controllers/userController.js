d97b2b23137877afa74b92c6b3e9d7edcac45ac0
Nikola Lazarov <nnl160@aubg.edu>
2020-02-28 09:25:57 +0200

    fix userController bug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -12,18 +12,18 @@ module.exports = () => {
   app.post('/api/users', (req, res) => {
     User.findOne({ email: req.body.user.email }, (error, user) => {
       // Handle database connection errors
-      if (error) return res.status(500).send(error.message);
+      if (error) return res.status(500).end(error.message);
 
       // Check if email already exists in the database
       if (user !== null) {
-        return res.status(500).send('A user already exists with this email.');
+        return res.status(500).end('A user already exists with this email.');
       }
 
       // Create new user and save to the database
       const newUser = new User(req.body.user);
       newUser.save((error) => {
-        if (error) res.status(500).send(error.message);
-        return res.send('User created successfully.');
+        if (error) res.status(500).end(error.message);
+        return res.status(200).end('User created successfully.');
       });
     });
   });
